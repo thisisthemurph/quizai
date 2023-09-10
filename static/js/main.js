04e91b6dca70d2b52cd35b326cd6a142c01f84cd
Mike Murphy <michael.w.muprhy@outlook.com>
2023-09-10 17:35:36 +0100

    Add normal (non-HTMX) spinner button.

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,20 @@ function removeElement(elem_id) {
     console.log(elem);
     elem.style.display = "none";
 }
+
+function showButtonSpinner(spinner_id, icon_id) {
+    const spinner = document.getElementById(spinner_id);
+    if (!spinner) {
+        return;
+    }
+
+    spinner.classList.remove('hidden');
+    spinner.classList.add('block');
+
+    if (icon_id) {
+        const icon = document.getElementById(icon_id);
+        if (icon) {
+            icon.classList.add('hidden');
+        }
+    }
+}
