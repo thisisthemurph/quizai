8dab00af10b0e43b5d0074b7ae05c9f8ec7385bc
Mike Murphy <michael.w.muprhy@outlook.com>
2023-09-16 09:03:42 +0100

    Adds simple navigation

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,19 @@ function removeElement(elem_id) {
     elem.style.display = "none";
 }
 
+function toggleElementClass(elem_id, className) {
+    const elem = document.getElementById(elem_id);
+    if (elem.classList.contains(className)) {
+        elem.classList.remove(className);
+    } else {
+        elem.classList.add(className);
+    }
+}
+
+function toggleHiddenEement(elem_id) {
+    toggleElementClass(elem_id, "hidden")
+}
+
 function showButtonSpinner(spinner_id, icon_id) {
     const spinner = document.getElementById(spinner_id);
     if (!spinner) {
